94356db8ac94f5a1c9acae3496eadcb4cc7b34b0
Stefan Schuermans <stefan@schuermans.info>
2020-08-18 22:17:46 +0200

    permission parsing

--- permissionerd/src/Permissions.cpp
+++ permissionerd/src/Permissions.cpp
@@ -3,7 +3,68 @@
 #include <string>
 
 void Permissions::parseParams(std::string const &paramStr) {
-  for (char c : paramStr) {
-    (void)c;
+  // reset permission flags
+  set = 0;
+  setCond = 0;
+  clear = 0;
+  clearCond = 0;
+  // parse comma-separated groups of [ugoa][+-][rwxX] settings
+  Flags who = 0, what = 0, whatCond = 0;
+  char action = ' ';
+  // append comma at end to process last group
+  for (char c : paramStr + ",") {
+    switch (c) {
+    case ',':
+      switch (action) {
+        case '=':
+          set = who * what;
+          setCond = who * whatCond;
+          clear = who * (what ^ (flagRead | flagWrite | flagExecute));
+          clearCond = who * (what ^ flagExecute);
+          break;
+        case '+':
+          set = who * what;
+          setCond = who * whatCond;
+          break;
+        case '-':
+          clear = who * what;
+          clearCond = who * whatCond;
+          break;
+      }
+      who = 0;
+      what = 0;
+      whatCond = 0;
+      action = ' ';
+      break;
+    case 'u':
+      who |= flagUser;
+      break;
+    case 'g':
+      who |= flagGroup;
+      break;
+    case 'o':
+      who |= flagOther;
+      break;
+    case 'a':
+      who |= flagUser | flagGroup | flagOther;
+      break;
+    case '=':
+    case '+':
+    case '-':
+      action = c;
+      break;
+    case 'r':
+      what |= flagRead;
+      break;
+    case 'w':
+      what |= flagWrite;
+      break;
+    case 'x':
+      what |= flagExecute;
+      break;
+    case 'X':
+      whatCond |= flagExecute;
+      break;
+    }
   }
 }
--- permissionerd/src/Permissions.h
+++ permissionerd/src/Permissions.h
@@ -1,11 +1,21 @@
 #ifndef PERMISSIONS_H
 #define PERMISSIONS_H
 
+#include <cstdint>
 #include <string>
 
 /// permissions configuration
 class Permissions {
 public:
+  typedef uint64_t Flags;
+
+  static const Flags flagRead = 01;
+  static const Flags flagWrite = 02;
+  static const Flags flagExecute = 04;
+  static const Flags flagOther = 01;
+  static const Flags flagGroup = 010;
+  static const Flags flagUser = 0100;
+
   /**
    * @brief parse permissions parameters
    * @param[in] parmStr parameter string
@@ -14,7 +24,10 @@ public:
   void parseParams(std::string const &paramStr);
 
 protected:
-  
+  Flags set = 0;       ///< permissions to set
+  Flags setCond = 0;   ///< permissions to set conditionally on usrexec/dir
+  Flags clear = 0;     ///< permissions to clear
+  Flags clearCond = 0; ///< permissions to clear conditionally on usrexec/dir
 };
 
 #endif // #ifndef PERMISSIONS_H
